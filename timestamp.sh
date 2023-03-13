f1e3dd3d9d5fd75699a327e1eea5042f66541d93
Mark Cooper <mark.c.cooper@outlook.com>
2023-03-13 15:04:45 -0700

    Update workflow to automate timestamp refresh

--- timestamp.sh
+++ timestamp.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-# if there has been no repository update in 60 days
-# GH actions will disable the test workflow, so this
-# can be used every now and again to trick it:
-# https://docs.github.com/en/actions/managing-workflow-runs/disabling-and-enabling-a-workflow
-echo `date +%s` > timestamp
+# update timestamp:
+# - records date of the last successful build
+# - keeps the workflow enabled
+echo "$(date) | $(date +%s)" > timestamp
